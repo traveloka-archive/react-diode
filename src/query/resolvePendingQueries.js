6d837bd97cc38222e235d80db371f7b919c0d8ef
Fatih <fatihkalifa@gmail.com>
2016-05-19 16:46:28 +0700

    Fixed infinite loop when using query with dependencies
    Using query with multi-level (not multiple) dependencies causing infinite
    loop because even though the query have many query dependencies, it can
    be resolved using initial response. The solution is try to recursively
    resolve query dependencies using initial response until the query cannot
    be resolved anymore.

--- src/query/resolvePendingQueries.js
+++ src/query/resolvePendingQueries.js
@@ -8,23 +8,35 @@ import type {
 
 import { generateQueryRequest } from './DiodeQueryRequest';
 
+function resolvePendingQuery(pendingQuery, response) {
+  const { callback, dependencies } = pendingQuery;
+
+  const resolvedDependencies = dependencies.map(query => {
+    return response[query.type];
+  }).filter(response => Boolean(response));
+
+  if (resolvedDependencies.length === dependencies.length) {
+    // all dependencies resolvedDependencies
+    const queryRequestInfo = callback(...resolvedDependencies);
+    const queryRequest = generateQueryRequest(pendingQuery, queryRequestInfo);
+
+    // After resolving initial query dependencies, it's possible that the query
+    // still return pending query which can be resolved using initial response.
+    if (queryRequest.dependencies) {
+      return resolvePendingQuery(queryRequest, response);
+    }
+
+    return queryRequest;
+  } else {
+    return pendingQuery;
+  }
+}
+
 export default function resolvePendingQueries(
   pendingQueries: Array<PendingQueryRequest>,
   response: any
 ): Array<DiodeQueryRequest> {
   return pendingQueries.map(pendingQuery => {
-    const { callback, dependencies } = pendingQuery;
-
-    const resolvedDependencies = dependencies.map(query => {
-      return response[query.type];
-    }).filter(response => Boolean(response));
-
-    if (resolvedDependencies.length === dependencies.length) {
-      // all dependencies resolvedDependencies
-      const queryRequestInfo = callback(...resolvedDependencies);
-      return generateQueryRequest(pendingQuery, queryRequestInfo);
-    } else {
-      return pendingQuery;
-    }
+    return resolvePendingQuery(pendingQuery, response);
   });
 }
--- test/query/resolvePendingQueries.js
+++ test/query/resolvePendingQueries.js
@@ -63,6 +63,72 @@ describe('resolvePendingQueries', () => {
     ).should.deep.equal(expectedQueries);
   });
 
+  it('should recursively resolve query dependencies', () => {
+    const resolveSpy = sinon.spy();
+    const pendingQueries = [
+      {
+        type: 'pending',
+        fragment: null,
+        callback(hotel) {
+          return {
+            type: 'pending',
+            fragment: null,
+            callback(user) {
+              return {
+                url: '/api/v1/pending',
+                method: 'post',
+                payload: {
+                  hotelId: hotel.id,
+                  userId: user.id
+                }
+              };
+            },
+            dependencies: [
+              {
+                type: 'user'
+              }
+            ]
+          };
+        },
+        dependencies: [
+          {
+            type: 'hotel'
+          }
+        ],
+        resolve: resolveSpy
+      }
+    ];
+    const responseMap = {
+      user: {
+        id: 2
+      },
+      hotel: {
+        id: 1,
+        name: '2'
+      }
+    };
+
+    const expectedQueries = [
+      {
+        type: 'pending',
+        url: '/api/v1/pending',
+        method: 'post',
+        fragment: null,
+        payload: {
+          hotelId: 1,
+          userId: 2
+        },
+        resolve: resolveSpy
+      }
+    ];
+
+    resolvePendingQueries(
+      pendingQueries,
+      responseMap
+    ).should.deep.equal(expectedQueries);
+  });
+
+
   it('should not resolve pending query if dependencies not met', () => {
     const resolveSpy = sinon.spy();
     const pendingQueries = [

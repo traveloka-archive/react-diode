bb78eb74e4abee477a948e58fce0a25dca5d3f4a
Juan Gunawan <juan.gunawan@traveloka.com>
2019-01-25 17:50:08 +0700

    Undo

--- examples/next/pages/index.js
+++ examples/next/pages/index.js
@@ -13,8 +13,8 @@ export default class Page extends React.Component {
       return { cache };
     }
 
-    // get cache from props
-    return { cache: this.props.cache };
+    // get cache from window
+    return { cache: window.__NEXT_DATA__.props.pageProps };
   }
 
   constructor(props) {

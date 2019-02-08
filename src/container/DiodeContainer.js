c401d7a39ad25685777dd7402455b2a75dcd929d
Juan Gunawan <juan.gunawan@traveloka.com>
2019-02-08 18:30:27 +0700

    Fix cache condition

--- src/container/DiodeContainer.js
+++ src/container/DiodeContainer.js
@@ -72,7 +72,10 @@ class DiodeQueryFetcher extends React.Component {
     // If cache is not provided, assume that all resources is already fetched
     // on the server.
     // NOTE: this will also prevent LoadingComponent to be rendered on server
-    if (!cache || Object.keys(cache).length === 0) {
+    if (
+      !cache ||
+      (typeof cache === "object" && Object.keys(cache).length === 0)
+    ) {
       if (process.env.NODE_ENV !== "production") {
         console.warn(
           "Cache not found. Rendering component without cache contents."
